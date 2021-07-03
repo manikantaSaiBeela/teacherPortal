5ccdfaeea1af73c2408795e9062edc706c7eb03f
Manikanta <manikantasai413.ms@gmail.com>
2021-07-03 18:52:12 +0530

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'layout', pathMatch:'full'},//Default route
+  {path: 'layout', component: LayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
